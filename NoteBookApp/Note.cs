21ab64cd3f85ba613aacb704696acd43a0231d38
Tedztar <tedztar@gmail.com>
2019-08-26 13:53:19 +1200

    stuff

---
+++ NoteBookApp/Note.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace NoteBookApp
+{
+    class Note
+    {
+        //Properties
+        private string title;
+        private string text;
+        private DateTime timestamp;
+
+        //Constructors
+        public Note(string title, string text)
+        {
+            this.title = title;
+            this.text = text;
+            this.timestamp = DateTime.Now;
+        }
+        //create the second contractor that had only 1 parameter: "title'
+        public Note(string title) : this(title, "") { }
+
+        //create the third constgructor: no parameter
+        public Note() : this("untitle") { }
+
+        //Methods
+        //Method toString()
+        public string toString()
+        {
+            return this.title + "" + this.text + "" + this.timestamp;
+        }
+        //setters and setters
+        public string getTitle()
+        {
+            return this.title;
+        }
+        public void setTitle(string title)
+        {
+            this.title = title;
+        }
+        public string getText()
+        {
+            return this.text;
+        }
+        public void setText(string text)
+        {
+            this.text = text;
+        }
+        public string getTimeStamp()
+        {
+            return this.timestamp.ToString();
+        }
+    }
+}
---
+++ NoteBookApp/noteCollection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NoteBookApp
+{
+    class noteCollection
+    {
+        //properties
+        private List<Note> noteList = new List<Note>();
+        //constructors
+        public noteCollection()
+        {
+            this.noteList = new List<Note>();//Initialize an empty list
+        }
+        //methods
+        //method listAll(): list information of all notes in the collection
+        public string listAll()
+        {
+            //declare a variable to store all info for the collection
+            string allNotes = "";
+            //loop through all notes in the collection
+            foreach(Note note in noteList)
+            {
+                allNotes += note.toString();
+            }
+            //Return value
+            return allNotes;
+        }
+
+        //method add(): add new note to the collection
+        public void add(Note newNote)
+        {
+            this.noteList.Add(newNote);
+        }
+        //method update(): update an existing note
+        public void update()
+        {
+
+        }
+        //method delete)(: delete an existing note
+        public void delete()
+        {
+
+        }
+    }
+}
